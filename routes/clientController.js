3391d2f6738d8dfbbe37eddc033dd193a08d8f40
Alyne Citta <alyne_fae@estudante.sesisenai.org.br>
2024-06-14 23:15:49 -0300

    Parte de funcionalidade de edição e exclusão de usuario cliente feito

--- routes/clientController.js
+++ routes/clientController.js
@@ -18,4 +18,52 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+      const clientId = parseInt(req.params.id)
+      const client = await pool.connect();
+      const result = await client.query(`
+      SELECT * FROM client WHERE id = $1`, [clientId]);
+      const rows = result.rows;
+      client.release();
+      res.json(rows);
+  
+    } catch (err) {
+      console.error('Erro ao executar a consulta', err);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  });
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const clientId = parseInt(req.params.id)
+      const client = await pool.connect();
+      const result = await client.query(`
+      DELETE FROM client WHERE id = $1`, [clientId]);
+      client.release();
+      console.log(result);
+      res.status(201).json({ message: 'Client deleted successfully' });
+  
+    } catch (err) {
+      console.error('Erro ao executar o delete', err);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  });
+
+  router.post('/:id', async (req, res) => {
+    try {
+      const clientId = parseInt(req.params.id)
+      const {name, email, password} = req.body;
+      const client = await pool.connect();
+      const result = await client.query(`
+      UPDATE client SET name = $2, email = $3, password = $4 WHERE id = $1`, [clientId, name, email, password]);
+      client.release();
+      res.status(201).json({ message: 'Client updated successfully' });
+  
+    } catch (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
 module.exports = router;
